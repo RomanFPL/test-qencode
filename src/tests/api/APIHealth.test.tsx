260d793b3e527222444a02e1361166c42ff9e64f
RomanFPL <roma.franko99@gmail.com>
2024-03-12 21:00:58 +0200

    build: :sparkles: tests

--- src/tests/api/APIHealth.test.tsx
+++ src/tests/api/APIHealth.test.tsx
@@ -1,4 +1,6 @@
-it('should return "detail": "OK" from the healthcheck API', async () => {
+import "@testing-library/jest-dom";
+
+test('should return "detail": "OK" from the healthcheck API', async () => {
   const response = await fetch("https://auth-qa.qencode.com/healthcheck");
   const data = await response.json();
 
